b125f38fac25f2fff233230c227b277be1c42a25
njenga <njengak@me.com>
2019-02-26 10:38:56 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES ('Mr.Chocoloate', 23, 'male', 'blue', 'happy', 1),
+      ('Rowdy', 26, 'male', 'blue', 'happy', 1),
+      ('Tabitha', 18, 'male', 'blue', 'happy', 1),
+      ('Sergeant Brown', 28, 'male', 'blue', 'happy', 1),
+      ('Melissa', 38, 'female', 'blue', 'happy', 1),
+      ('Grinch', 48, 'female', 'blue', 'happy', 1),
+      ('Wendy', 58, 'female', 'blue', 'happy', 1),
+      (NULL, 78, 'female', 'blue', 'happy', 1);
