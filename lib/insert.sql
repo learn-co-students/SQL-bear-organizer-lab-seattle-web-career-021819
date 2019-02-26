8acf4e3938862a4c48900b0f7bd800e4b3a71aed
isalevine <isaacdlevine@gmail.com>
2019-02-26 13:19:29 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 1, "gay", "brown", "ornery", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 1, "queer", "brown", "hungry", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 1, "female", "brown", "sleepy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Seargent Brown", 2, "enby", "black", "lazy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 2, "female", "black", "happy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 2, "demi-boy", "green", "jealous", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 3, "female", "white", "chill", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 4, "genderfluid", "green", "defensive", 1);
